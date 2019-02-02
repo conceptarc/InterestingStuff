4f19be19627c8e874b126e4d728b66a3ba0fb6e3
Henry <henryli1994@hotmail.com>
2019-02-02 11:55:23 -0500

    Added new user actions - added display & update functionality for the main data grid - added button and hotkey [N] for creating a new time tracking entry - added button and hotkey [S] for starting or stopping the timer for the currently selected entry

--- TimeTrack/TimeTrackingApp/TimeTrackingApp/MainWindow.xaml.cs
+++ TimeTrack/TimeTrackingApp/TimeTrackingApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Drawing;
+using System.Threading;
 using Hardcodet.Wpf;
 using Hardcodet.Wpf.TaskbarNotification;
 
@@ -24,13 +26,22 @@ namespace TimeTrackingApp
     public partial class MainWindow : Window
     {
         private TaskbarIcon taskBarIcon;
+        private ObservableCollection<TimeEntryView> viewCollection;
+        private List<TimeEntry> currentTimeEntries;
+        private Dictionary<int, TimeEntry> entryByIds;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            #region Setup the taskbar icon and its right-click menu option.
+            InitTaskbar();
 
+            InitDataGrid();
+        }
+
+        // Set up the taskbar icon and its right-click menu option
+        private void InitTaskbar()
+        {
             ContextMenu rightClickMenu = new ContextMenu();
             MenuItem closeOption = new MenuItem { Header = "Close" };
             closeOption.Click += new RoutedEventHandler(
@@ -59,10 +70,136 @@ namespace TimeTrackingApp
             taskBarIcon.TrayBalloonTipClicked += new RoutedEventHandler(
                 (object sendr, RoutedEventArgs eventArgs) =>
                 {
-                    this.Show(); // same as TrayMouseDoubleClick
+                    //this.Show(); // same as TrayMouseDoubleClick
                 });
+        }
 
-            #endregion
+        // Set up the data grid
+        private void InitDataGrid()
+        {
+            TableGrid.IsReadOnly = true;
+            TableGrid.SelectionMode = DataGridSelectionMode.Single;
+            TableGrid.MinRowHeight = 50;
+
+            viewCollection = new ObservableCollection<TimeEntryView>();
+            currentTimeEntries = LoadTimeEntries();
+            entryByIds = new Dictionary<int, TimeEntry>();
+
+            RefreshViewCollection();
+
+            this.TableGrid.ItemsSource = viewCollection;
+
+            Task.Factory.StartNew(() =>
+            {
+                RenderTableGrid();
+            });
+
+            // Delayed configuration of column width to fit the last column to the end of the screen.
+            Task.Factory.StartNew(() =>
+            {
+                while (this.TableGrid.Columns.Count == 0)
+                {
+                    Thread.Sleep(100);
+                }
+                this.Dispatcher.Invoke(() =>
+                {
+                    // [3] is Details
+                    this.TableGrid.Columns[3].Width = new DataGridLength(70, DataGridLengthUnitType.Star);
+
+                    //https://stackoverflow.com/questions/35543044/wpf-datagrid-column-textwrapping-programmatically
+                    var style = new Style(typeof(TextBlock));
+                    style.Setters.Add(new Setter(TextBlock.TextWrappingProperty, TextWrapping.Wrap));
+                    // style.Setters.Add(new Setter(TextBlock.VerticalAlignmentProperty, VerticalAlignment.Center));
+
+                    (TableGrid.Columns[3] as DataGridTextColumn).ElementStyle = style;
+                });
+            });
+        }
+
+        private void RefreshViewCollection()
+        {
+            // Merge / update differences between the currentTimeEntries and viewCollection
+
+            // Use for-int so we can modify the collection.
+            for (int i = 0; i < viewCollection.Count; i++)
+            {
+                TimeEntryView view = viewCollection[i];
+                int id = view.GetId();
+                if (!entryByIds.ContainsKey(id))
+                    continue;
+
+                TimeEntryView newView = entryByIds[id].ToView(id);
+                view.Active = newView.Active;
+                view.Hours = newView.Hours;
+                view.Name = newView.Name;
+                view.Details = newView.Details;
+            }
+
+            // Insert new
+            foreach (TimeEntry entry in currentTimeEntries)
+            {
+                if (entryByIds.ContainsValue(entry))
+                    continue;
+
+                int newId = entryByIds.Keys.Count + 1;
+                entryByIds[newId] = entry;
+                viewCollection.Add(entry.ToView(newId));
+            }
+
+            //viewCollection.Clear();
+            //foreach (TimeEntry entry in currentTimeEntries)
+            //{
+            //    viewCollection.Add(entry.ToView());
+            //}
+        }
+
+        private void RenderTableGrid()
+        {
+            while (true)
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    RefreshViewCollection();
+                    //TableGrid.Items.Refresh();
+                    //CollectionViewSource.GetDefaultView(TableGrid.ItemsSource).Refresh();
+                });
+                Thread.Sleep(50);
+            }
+        }
+
+        private List<TimeEntry> LoadTimeEntries()
+        {
+#warning test data for now
+            return new List<TimeEntry>
+            {
+                new TimeEntry
+                {
+                    Name = "test1",
+                    Details = "developing this task and then logging my time into Jira.." +
+                        ".\r\n- continuing after lunch",
+                    TimePeriods = new List<TimePeriod>
+                    {
+                        new TimePeriod
+                        {
+                            StartTime = DateTime.Now.AddHours(-2.5),
+                            EndTime = DateTime.Now.AddHours(-1.5)
+                        }
+                    }
+                },
+                new TimeEntry
+                {
+                    Name = "task2",
+                    Details = "reading this textbook before committing the code.." +
+                        ".\r\n- continuing after lunch",
+                    TimePeriods = new List<TimePeriod>
+                    {
+                        new TimePeriod
+                        {
+                            StartTime = DateTime.Now.AddHours(-1.5)
+                        }
+                    }
+                }
+            };
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
@@ -72,7 +209,86 @@ namespace TimeTrackingApp
             this.Hide();
 
             taskBarIcon.ShowBalloonTip("Time Tracker App",
-                "Minimized to task tray. Click here to undo.", BalloonIcon.Info);
+                "Minimized to task tray.", BalloonIcon.Info);
         }
+
+        #region Create new time tracking entry
+
+        private void NewEntry_Click(object sender, RoutedEventArgs e)
+        {
+            CreateNewEntry();
+        }
+
+        private void WindowScreen_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.N)
+                CreateNewEntry();
+
+            if (e.Key == Key.S) // start / stop
+                ToggleEntryTimer();
+        }
+
+        private void CreateNewEntry()
+        {
+            TimeEntry activeEntry = currentTimeEntries.FirstOrDefault(m => m.IsActive);
+            if (activeEntry != null)
+                activeEntry.StopTimer();
+
+            TimeEntry newEntry = new TimeEntry
+            {
+                Name = "test click",
+                Details = "test test test"
+            };
+            currentTimeEntries.Add(newEntry);
+            newEntry.StartTimer();
+        }
+
+        #endregion
+
+        #region Switch timer
+
+        private void TableGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void TableGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //TimeEntryView selectedViewEntry = e.AddedItems[0] as TimeEntryView;
+            //TimeEntry newEntry = entryByIds[selectedViewEntry.GetId()];
+
+            //SwitchTimer(newEntry);
+        }
+
+        private void ToggleTimer_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleEntryTimer();
+        }
+
+        private void ToggleEntryTimer()
+        {
+            if (TableGrid.SelectedItem == null)
+                return;
+
+            TimeEntryView selectedViewEntry = TableGrid.SelectedItem as TimeEntryView;
+            TimeEntry selectedEntry = entryByIds[selectedViewEntry.GetId()];
+
+            if (selectedEntry.IsActive)
+                selectedEntry.StopTimer();
+            else
+                SwitchTimer(selectedEntry);
+        }
+
+        private void SwitchTimer(TimeEntry newEntry)
+        {
+            TimeEntry activeEntry = currentTimeEntries.FirstOrDefault(m => m.IsActive);
+            if (activeEntry != null)
+                activeEntry.StopTimer();
+
+            newEntry.StartTimer();
+        }
+
+        #endregion
+
     }
 }
---
+++ TimeTrack/TimeTrackingApp/TimeTrackingApp/TimeEntry.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace TimeTrackingApp
+{
+    public class TimeEntry
+    {
+        public string Name { get; set; }
+        public string Details { get; set; }
+
+        private List<TimePeriod> timePeriods;
+        public List<TimePeriod> TimePeriods
+        {
+            get
+            {
+                if (timePeriods == null)
+                    timePeriods = new List<TimePeriod>();
+                return timePeriods;
+            }
+            set
+            {
+                timePeriods = value;
+            }
+        }
+
+        public bool IsActive
+        {
+            get
+            {
+                TimePeriod lastPeriod = TimePeriods.LastOrDefault();
+                return lastPeriod != null && !lastPeriod.EndTime.HasValue;
+            }
+        }
+
+        public decimal DurationHours
+        {
+            get
+            {
+                decimal totalHours = Convert.ToDecimal(this.GetDuration().TotalHours);
+                // don't deal with double imprecision
+                return Math.Round(totalHours, 4);
+            }
+        }
+
+        public TimeSpan GetDuration()
+        {
+            TimeSpan duration = new TimeSpan();
+
+            foreach (TimePeriod period in TimePeriods)
+            {
+                DateTime endTime = period.EndTime ?? DateTime.Now;
+                TimeSpan currentDuration = endTime.Subtract(period.StartTime);
+
+                duration = duration.Add(currentDuration);
+            }
+
+            return duration;
+        }
+
+        //public decimal GetDurationHours(int decimalPrecision = 2)
+        //{
+        //    decimal totalHours = Convert.ToDecimal(this.GetDuration().TotalHours);
+        //    // don't deal with double imprecision
+        //    return Math.Round(totalHours, decimalPrecision);
+        //}
+
+        public void StartTimer()
+        {
+            // Check if the last time period is still running. If so do nothing.
+            TimePeriod lastPeriod = TimePeriods.LastOrDefault();
+            if (lastPeriod != null && !lastPeriod.EndTime.HasValue)
+                return;
+
+            // Otherwise add a new timing period.
+            TimePeriods.Add(new TimePeriod { StartTime = DateTime.Now });
+        }
+
+        public void StopTimer()
+        {
+            // Close the last time period if existing.
+            TimePeriod lastPeriod = TimePeriods.LastOrDefault();
+            if (lastPeriod != null && !lastPeriod.EndTime.HasValue)
+                lastPeriod.EndTime = DateTime.Now;
+        }
+
+        public TimeEntryView ToView(int id)
+        {
+            var view = new TimeEntryView
+            {
+                Active = IsActive,
+                Hours = DurationHours.ToString("0.00000"),
+                Name = Name,
+                Details = Details
+            };
+            view.SetId(id);
+
+            return view;
+        }
+    }
+
+    public class TimePeriod
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+    }
+
+    // This class is for data grid binding & display.
+    public class TimeEntryView : INotifyPropertyChanged
+    {
+        private int Id;
+
+        private bool active;
+        public bool Active
+        {
+            get { return active; }
+            set
+            {
+                active = value;
+                OnPropertyChanged("Active");
+            }
+        }
+
+        private string hours;
+        public string Hours
+        {
+            get { return hours; }
+            set
+            {
+                hours = value;
+                OnPropertyChanged("Hours");
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        private string details;
+        public string Details
+        {
+            get { return details; }
+            set
+            {
+                details = value;
+                OnPropertyChanged("Details");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+
+        public int GetId()
+        {
+            return Id;
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
